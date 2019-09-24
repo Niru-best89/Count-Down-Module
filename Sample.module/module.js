938b2a573ba3411965dd05ba025c25091d730e48
Niru-best89 <55746284+Niru-best89@users.noreply.github.com>
2019-09-24 18:39:45 +0530

    Update module.js

--- Sample.module/module.js
+++ Sample.module/module.js
@@ -1,1 +1,18 @@
 // js
+var date = $('.countDate').attr('src');
+
+const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+let countDown = new Date(date).getTime(),
+    x = setInterval(function() {
+
+      let now = new Date().getTime(),
+          distance = countDown - now;
+
+      document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);  }, second)
